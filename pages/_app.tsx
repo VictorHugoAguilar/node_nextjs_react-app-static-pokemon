08ed3b05e90c0273626f30088dd860f43d73e26e
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-14 20:25:41 +0200

    create theme for dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/react';
+
+import { darkTheme } from '../themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<NextUIProvider theme={darkTheme}>
+    <Component {...pageProps} />
+  </NextUIProvider>);
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>first page</h1>
+      <Button color="gradient"> Bola</Button>
     </>
   )
 }
