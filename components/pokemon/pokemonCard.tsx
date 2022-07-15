ec19f394a3d65386136d94a12fec9c4a81e27be7
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-15 19:04:17 +0200

    create page for pokemon design

--- components/pokemon/pokemonCard.tsx
+++ components/pokemon/pokemonCard.tsx
@@ -1,4 +1,5 @@
 import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { SmallPokemon } from "../../interface/pokemon-list";
 
@@ -6,20 +7,26 @@ import { SmallPokemon } from "../../interface/pokemon-list";
 interface Props {
     pokemon: SmallPokemon
 }
-export const PokemonCard: FC<Props> = ({ pokemon: { id, img, name } }) => {
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
+
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/pokemon/${pokemon.id}`)
+    }
 
     return (
-        <Grid xs={6} sm={3} xl={1} key={id}>
-            <Card>
+        <Grid xs={6} sm={3} xl={1} key={pokemon.id}>
+            <Card isHoverable isPressable onClick={onClick}>
                 <Card.Body css={{ p: 1 }}>
-                    <Card.Image src={img!}
+                    <Card.Image src={pokemon.img!}
                         width="100%"
                         height={140} />
                 </Card.Body>
                 <Card.Footer>
                     <Row justify='space-between'>
-                        <Text transform='capitalize'>{name}</Text>
-                        <Text># {id}</Text>
+                        <Text transform='capitalize'>{pokemon.name}</Text>
+                        <Text># {pokemon.id}</Text>
                     </Row>
                 </Card.Footer>
             </Card>
