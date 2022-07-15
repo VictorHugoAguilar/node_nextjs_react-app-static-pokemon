757a1fc330453c755ff6d0f60f5fc8ab82913dc4
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-15 17:21:21 +0200

    create card and grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@nextui-org/react';
+import { Button, Card, Grid, Row, Text } from '@nextui-org/react';
 import type { GetStaticProps, NextPage } from 'next';
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
@@ -12,8 +12,39 @@ const HomePage: NextPage<Props> = (props) => {
 
   console.log(props)
 
+  const { pokemons } = props;
+
   return (
     <Layout title='Listado de Pokémons'>
+
+      <Grid.Container gap={2} justify='flex-start'>
+
+        {
+          pokemons.map(({ id, name, img }) => (
+
+            <Grid xs={6} sm={3} xl={1} key={id}>
+              <Card>
+                <Card.Body css={{ p: 1 }}>
+                  <Card.Image src={img}
+                    width="100%"
+                    height={140} />
+                </Card.Body>
+                <Card.Footer>
+                  <Row justify='space-between'>
+                    <Text transform='capitalize'>{name}</Text>
+                    <Text># {id}</Text>
+                  </Row>
+                </Card.Footer>
+
+              </Card>
+
+            </Grid>
+          ))
+
+
+        }
+
+      </Grid.Container>
 
 
 
